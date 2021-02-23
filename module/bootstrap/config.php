359c669696dcad45a419383df81a047e50804843
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-02-23 11:00:37 +0100

    Zaktualizowano moduł do wersji 1.2
    Zaktualizowano moduł do wersji 1.2
    Dodano funkcję wczytującą skrypt dataTable

--- module/bootstrap/config.php
+++ module/bootstrap/config.php
@@ -2,7 +2,7 @@
 return [
 	'include' => [],
 	'moduleFile' => 'main.php',
-	'version' => '1.1',
+	'version' => '1.2',
 	'uniqueID' => '984098a692bb65199be0d4cf6f1f1ebc'
 ];
 ?>
--- module/bootstrap/main.php
+++ module/bootstrap/main.php
@@ -1,16 +1,30 @@
 <?php
-return new class(){ //create class
-	public function __construct(){ //construct
-		$config = core::$module_add['bootstrap']['config']; //load config to variable
-		$module_list = array_keys(core::$module_add); //module list
-		$search = array_search('smarty', $module_list); //search smarty
-		if($search === false) //if smarty not exists
-			return core::setError(1, 'smarty not exists'); //return error 1
-		$smarty = core::$module['smarty']->smarty; //get smarty
-		$bootstrap = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-		<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>';
-		$smarty->assign('bootstrap', $bootstrap); //assign bootstrap
+return new class(){
+	public $headerData = [
+		'bootstrapCSS' => '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">',
+		'JQuery' => '<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>',
+		'BootstrapJS' => '<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>'
+	];
+	public function __construct(){
+		$module_list = array_keys(core::$module_add);
+		if(is_bool(array_search('smarty', $module_list)))
+			return core::setError(1, 'smarty not exists');
+		$this->generateHTMLData();
+	}
+	public function generateHTMLData(){
+		$bootstrap = '';
+		foreach($this->headerData as $data)
+			$bootstrap .= $data;
+		core::$module['smarty']->smarty->assign('bootstrap', $bootstrap);
+	}
+	public function loadBootstrapDataTable($multipleTable=false){
+		$this->headerData['dataTablesCSS'] = '<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css"/>';
+		$this->headerData['dataTablesJqueryJS'] = '<script type="text/javascript" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>';
+		$this->headerData['dataTablesBootstrapJS'] = '<script type="text/javascript" src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>';
+		$this->headerData['dataTablesStyle'] = '<style> .dataTables_wrapper .row:last-child{ padding: 10px; } .dataTables_wrapper .row:first-child{ padding: 10px; padding-bottom: 0px; } </style>';
+		if($multipleTable)
+			$this->headerData['dataTablesMultipleTableJS'] = '<script> $(document).ready(function() { $(\'table.dataTable\').DataTable({ "language": { "url": "https://cdn.datatables.net/plug-ins/1.10.22/i18n/Polish.json" }, stateSave: true }); }); </script>';
+		$this->generateHTMLData();
 	}
 }
 ?>
