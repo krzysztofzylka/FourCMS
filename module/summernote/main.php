8d7b73758c11ce7833d43c04eb4405031404a8e9
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-03-30 12:35:42 +0200

    Zaktualizowano moduł summernote do wersji 1.1
    Ograniczenie zużycia transferu oraz dysku serwera przez pobieranie skryptów z serwerów cdnjs

--- module/summernote/main.php
+++ module/summernote/main.php
@@ -7,9 +7,9 @@ return new class(){
 		if($search === false) return core::setError(1, 'smarty not exists');
 		$search = array_search('bootstrap', $module_list);
 		if($search === false) return core::setError(1, 'bootstrap not exists');
-		$summernote = '<link rel="stylesheet" href="'.$config['path'].'script/summernote-bs4.css">
-		<script src="'.$config['path'].'script/summernote-bs4.js"></script>
-		<script src="'.$config['path'].'script/lang/summernote-pl-PL.min.js"></script>';
+		$summernote = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/lang/summernote-pl-PL.min.js"></script>';
 		core::$module['smarty']->smarty->assign('summernote', $summernote);
 	}
 }
