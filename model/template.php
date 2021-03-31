7021e3c5f8a088a10772c4ffab90f04c6b2897e3
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-03-31 06:43:09 +0200

    Dodano funkcję wspomagające obsługę szablonów
    - Dodano funkcję display, scanModuleResource oraz __getDirContents (prywatna)

--- model/template.php
+++ model/template.php
@@ -3,6 +3,7 @@ return new class() {
     public $templateDir = null;
     public $templateName = null;
     public $defaultTemplateDir = 'defaultBlog';
+    private $extendsListAdminPanel = null;
 	public function __construct(){
 		core::setError();
         $this->templateName = core::$model['config']->read('template_name');
@@ -34,6 +35,44 @@ return new class() {
             }
         }
         return $return;
+    }
+    public function display($fileName){
+        if(is_null($this->extendsListAdminPanel))
+            $this->scanModuleResource();
+        if(isset($this->extendsListAdminPanel[$fileName])){
+            $extendsData = implode('|', $this->extendsListAdminPanel[$fileName]);
+            core::$module['smarty']->smarty->addTemplateDir('../module/');
+            core::$model['template']->display('extends:'.$fileName.'|'.$extendsData);
+        }else
+            core::$module['smarty']->smarty->display($fileName);
+    }
+    public function scanModuleResource(){
+        $extendsListAdminPanel = [];
+        foreach(core::$library->module->moduleList(true) as $moduleData){
+            $modulePath = $moduleData['path'];
+            $resourceTemplateDir = $modulePath.'Resources/adminPanel/template/';
+            if(file_exists($resourceTemplateDir)){
+                foreach($this->__getDirContents($resourceTemplateDir) as $resourcePath){
+                    if(!is_dir($resourcePath)){
+                        $extendsListAdminPanel[str_replace($modulePath.'Resources/adminPanel/', '', $resourcePath)][] = str_replace('../module/', '', $resourcePath);
+                    }
+                }
+            }
+        }
+        $this->extendsListAdminPanel = $extendsListAdminPanel;
+    }
+    private function __getDirContents($dir, &$results = array()) {
+        $files = scandir($dir);
+        foreach ($files as $key => $value) {
+            $path = core::$library->file->repairPath($dir . DIRECTORY_SEPARATOR . $value);
+            if (!is_dir($path))
+                $results[] = $path;
+            elseif($value != "." && $value != "..") {
+                $this->__getDirContents($path, $results);
+                $results[] = $path;
+            }
+        }
+        return $results;
     }
 };
 ?>
