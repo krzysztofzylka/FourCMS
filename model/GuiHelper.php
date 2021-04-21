0262cbffbc75bf821588849d7841ba0d9e260faa
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-04-21 17:08:53 +0200

    Poprawiono model oraz zaktualizowano funkcję ajaxLink

--- model/GuiHelper.php
+++ model/GuiHelper.php
@@ -1,6 +1,6 @@
 <?php
 return new class(){
-    public function contentAlert(
+	public function contentAlert(
         string $value,
         string $alertType = 'success',
         string $title = null
@@ -8,24 +8,23 @@ return new class(){
         core::setError();
         
         echo '<div class="content pt-2">
-            <div class="alert alert-'.$alertType.'">
-                '.(($title <> null)?'<h4 class="alert-heading">'.$title.'</h4>':'').'
-                '.$value.'
+            <div class="alert alert-' . $alertType . '">
+                ' . (($title <> null) ? '<h4 class="alert-heading">' . $title . '</h4>' : '') . '
+                ' . $value . '
             </div>
         </div>';
     
         return;
     }
-    public function alert(
+	public function alert(
         string $value,
         string $alertType = 'success',
         string $title = null
     ){
         core::setError();
         
-        echo '<div class="alert alert-'.$alertType.'">
-            '.(is_null($title)?'':'<h4 class="alert-heading">'.$title.'</h4>').'
-            '.$value.'
+        echo '<div class="alert alert-' . $alertType . '">
+            ' . (is_null($title) ? '' : '<h4 class="alert-heading">' . $title . '</h4>') . $value . '
         </div>';
 
         return;
@@ -36,23 +35,24 @@ return new class(){
 		$inputName='link'
 	){
 		core::setError();
-		$explode = ($actual == '')?['', '']:(explode('-', $actual, 2));
+
+		$explode = ($actual == '') ? ['', ''] : (explode('-', $actual, 2));
 
 		//HTML
 		echo '<div class="form-row">
 			<div class="form-group col-md-4">
 				<select class="form-control" id="linkGenerator_main">
 					<option selected>Wybierz</option>
-					'.(!is_bool(array_search('post', $showList))?'<option value="post">Post</option>':'').'
-					'.(!is_bool(array_search('module', $showList))?'<option value="module">Moduł</option>':'').'
-					'.(!is_bool(array_search('link', $showList))?'<option value="link">Link</option>':'').'
-					'.(!is_bool(array_search('controller', $showList))?'<option value="controller">Kontroler</option>':'').'
+					' . (!is_bool(array_search('post', $showList)) ? '<option value="post">Post</option>' : '') . '
+					' . (!is_bool(array_search('module', $showList)) ? '<option value="module">Moduł</option>' : '') . '
+					' . (!is_bool(array_search('link', $showList)) ? '<option value="link">Link</option>' : '') . '
+					' . (!is_bool(array_search('controller', $showList)) ? '<option value="controller">Kontroler</option>' : '') . '
 				</select>
 			</div>
 			<div class="form-group col-md-8">
 				<select class="custom-select" id="linkGenerator_slave"><option selected>#</option></select>
 				<input type="text" class="form-control" id="linkGenerator_slave2" placeholder="Link" value="#">
-				<input type="text" id="link" name="'.$inputName.'" value="#"></input>
+				<input type="text" id="link" name="' . $inputName . '" value="#"></input>
 			</div>
 		</div>';
 
@@ -61,39 +61,53 @@ return new class(){
 
 		//JavaScript
 		echo '<script>
-		var auto1 = "'.(isset($explode[0])?$explode[0]:'Wybierz').'";
-        var auto2 = "'.(isset($explode[1])?$explode[0].'-'.$explode[1]:'').'";
+		var auto1 = "' . (isset($explode[0]) ? $explode[0] : 'Wybierz') . '";
+        var auto2 = "' . (isset($explode[1]) ? $explode[0] . '-' . $explode[1] : '') . '";
 		var controller_list = [';
-		foreach(array_diff(scandir('../controller/'), ['.', '..', '.htaccess']) as $item)
-			echo '"'.str_replace('.php', '', $item).'", ';
+		foreach (array_diff(scandir('../controller/'), ['.', '..', '.htaccess']) as $item) {
+			echo '"' . str_replace('.php', '', $item) . '", ';
+		}
 		echo '];
 		var post_list = [["#", "--- Pusty ---"],';
-		foreach(core::$model->Post->list() as $item)
-			echo '["post-'.$item['id'].'.html", "'.$item['title'].'"],';
+		foreach (core::$model->Post->list() as $item) {
+			echo '["post-' . $item['id'] . '.html", "' . $item['title'] . '"],';
+		}
 		echo '];
         var module_list = [';
-		foreach(core::$model->Module->moduleDisplayPageList() as $item)
-			echo '["module-'.$item['name'].'.html", "'.$item['title'].'"],';
+		foreach (core::$model->Module->moduleDisplayPageList() as $item) {
+			echo '["module-' . $item['name'] . '.html", "' . $item['title'] . '"],';
+		}
 		echo '];
 		</script>';
 
 		//load javascript script
-		echo file_exists('script/linkGenerator.js')?'<script src="script/linkGenerator.js"></script>':'<script src="../script/linkGenerator.js"></script>';
+		echo file_exists('script/linkGenerator.js') ? '<script src="script/linkGenerator.js"></script>' : '<script src="../script/linkGenerator.js"></script>';
 	}
 	public function ajaxLink(
-		string $controllerName,
+		string $controllerModelName,
 		string $urlName,
 		string $getData = null,
-		string $url = '#'
+		string $url = '#',
+		string $type = 'link',
+		string $class = ''
 	) {
-		return '<a href="'.$url.'" onclick="ajaxLink(\'' . $controllerName . ($getData<>null ? '?' . $getData : '') . '\')">' . $urlName . '</a>';
+		switch ($type) {
+			case 'ajaxLink':
+				return $controllerModelName . ($getData<>null ? '?' . $getData : '');
+				break;
+			case 'link':
+			default:
+				return '<a href="' . $url . '" class="' . $class . '" onclick="ajaxLink(\'' . $controllerModelName . ($getData<>null ? '?' . $getData : '') . '\')">' . $urlName . '</a>';
+				break;
+		}
+
 	}
 	public function toast(
 		string $text,
 		string $type = 'info'
 	) {
 		echo '<script>
-			toastr.'.$type.'("'.$text.'");
+			toastr.' . $type . '("' . $text . '");
 		</script>';
 	}
 }
